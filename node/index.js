bc9020390dd3a747f123541e581d282dace04311
Adam Butler <adam@lab.io>
2015-03-22 23:58:50 +0000

    compile and log code

--- node/index.js
+++ node/index.js
@@ -1,9 +1,24 @@
+var fs = require('fs');
+var program = require("commander");
+var mkdirp = require("mkdirp")
 var js2coffee = require("js2coffee");
-
-var input = "function foo() { console.log('bar'); }";
 
 var result = js2coffee.build(input);
 
-var program = require("commander");
+var input = process.argv[2];
 
 program.version("0.0.1").option("-o, --output", "Output path");
+
+if (typeof input === "undefined" || input === null) {
+  console.log("You must provide an input as the first argument");
+}
+
+fs.readFile(input, "utf-8", function(error, data) {
+  coffee = js2coffee.build(data).code;
+  if (program.output != null) {
+    // output
+  } else {
+    console.log(coffee);
+  }
+});
+
